06bec21236bcb5eb96881ccbfe6ae5d225076dde
ahmedsame669-create <ahmedsame669@gmail.com>
2026-08-25 00:34:18 +0300

    Add service worker for caching and offline functionality
    Implement service worker for caching files and offline support.

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "talabe-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json"
+];
+
+// تثبيت ملفات الكاش
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// جلب البيانات (العمل بدون إنترنت جزئياً)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
