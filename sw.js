ac30f6a8926084fa1f76c395ab507ca438d8d3a0
ahmedsame669-create <ahmedsame669@gmail.com>
2026-08-26 21:04:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAmpaAcMsQsJyekFAFvhJ-rLILSPUMtdJA",
-  authDomain: "talabe-app.firebaseapp.com",
-  projectId: "talabe-app",
-  storageBucket: "talabe-app.firebasestorage.app",
-  messagingSenderId: "552943090500",
-  appId: "1:552943090500:web:79cfcd45bea5185cda3511"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
